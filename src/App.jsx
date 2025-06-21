d0c308178914bf7a6e1b550bdd9c5e16a186e124
Uzair Atiff <uzairatif911@gmail.com>
2025-06-21 15:07:08 +0500

    Updated code with new features/fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route , Outlet } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import './App.css'
 import Login from './components/auth/Login'
@@ -9,7 +9,8 @@ import AddBlog from './components/blogApp/addBlog';
 import UserBlogs from './components/blogApp/UserBlogs';
 import AllBlogs from './components/blogApp/AllBlogs';
 import UpdateForm from './components/blogApp/updateForm';
-
+import PrivateRoute from './routes/PrivateRoute';
+import AuthRoute from './routes/AuthRoute';
 
 function App() {
 
@@ -17,14 +18,20 @@ function App() {
   return (
     <>
       <Routes>
+
+        <Route element={<AuthRoute />}>
         <Route path='/' element={<Signup />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Signup' element={<Signup />} />
+        </Route>
+
+        <Route element ={<PrivateRoute />}>
         <Route path="/Allblogs" element={<AllBlogs />} />
         <Route path="/updateForm" element={<UpdateForm />} />
         <Route path='/home' element={<Home />} />  {/* Add this line */}
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Signup' element={<Signup />} />
         <Route path="/addBlog" element={<AddBlog />} />
         <Route path="/UserBlogs" element={<UserBlogs />} />
+        </Route>
 
       </Routes>
       <ToastContainer
